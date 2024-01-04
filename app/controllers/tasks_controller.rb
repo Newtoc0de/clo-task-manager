9bcbdcbe0105edfc97a31c15b79891959eda8850
Clothilde Drouet clothildedrouet@gmail.com <clothildedrouet@gmail.com>
2024-01-04 17:33:12 +0100

    delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -21,8 +21,13 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task.update(task_param)
+    @task.update(task_params)
     redirect_to @task
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
